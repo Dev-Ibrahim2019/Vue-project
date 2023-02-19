4b6cacdc02fdfb1d4771d780d865059ba0774263
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-02-19 22:12:47 +0200

    Vue with options Api style

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<!-- <script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script> -->
+
+<script>
+import TheWelcome from "../components/TheWelcome.vue";
+
+export default {
+  components: { TheWelcome },
+  data() {
+    return {
+      message: "Hi there!",
+    };
+  },
+  mounted() {
+    alert("I have been mounted");
+  },
+};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{ message }}</p>
+  </main>
+</template>
