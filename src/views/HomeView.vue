1b555de6b246b81744f4b66d095d68bc303b6cc1
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-02-22 04:54:18 +0200

    Composable working with localStorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { useFlash } from "@/composables/useFlash.js";
+import { useStorage } from "@/composables/useStorage.js";
 
-let { flash } = useFlash();
+let food = useStorage("food", "pizza");
+let age = useStorage("age", 10);
+
+let obj = useStorage("obj", { one: "one" });
+
+setTimeout(() => {
+  obj.value.one = "CHANGED";
+}, 3000);
 </script>
 
 <template>
   <main>
     <p>
-      <button @click="flash('Test', 'It Works!')">Click me</button>
+      What is your favorite food?
+      <input type="text" v-model="food" />
+    </p>
+    <p>
+      How old are you?
+      <input type="text" v-model="age" />
     </p>
   </main>
 </template>
